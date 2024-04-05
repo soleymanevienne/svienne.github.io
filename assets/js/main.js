f623e3c5243b870c9a6edb4135ee68da8dfc2f73
soleymanevienne <123655159+soleymanevienne@users.noreply.github.com>
2024-04-05 14:12:14 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,8 +51,8 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .about__buttons-container',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .section-container',{delay: 400}); 
 sr.reveal('.home__social-icon,  .home__social-info',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 sr.reveal('.section-title', {
